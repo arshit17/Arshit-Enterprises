899feb323ad7c43e1e0dd60a5040ae68193f22f7
Arshit Mittal <135101044+arshit17@users.noreply.github.com>
2026-06-08 14:47:10 +0530

    Update CORS origin to specific URL

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "https://arshit-enterprises.vercel.app",
     credentials: true
 }));
 app.use(bodyParser.json());
